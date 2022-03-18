4fcf47f4e74bf68c78bc5c0632414331a0518699
Augustine Awuori <augutineawuroi95@gmail.com>
2022-03-18 18:03:02 +0300

    Build account screen.

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import Card from './app/components/Card';
 import Screen from './app/components/Screen';
+import AccountScreen from './app/screens/AccountScreen';
 import EstablishmentsScreen from './app/screens/EstablishmentsScreen';
 
 export default function App() {
   return (
-    <EstablishmentsScreen />
+    <AccountScreen />
   );
 }
---
+++ app/screens/AccountScreen.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { FlatList, StyleSheet, View } from "react-native";
+import ListItem from "../components/lists/ListItem";
+
+import colors from "../config/colors";
+import Screen from "../components/Screen";
+import ListItemSeparator from '../components/lists/ListItemSeparator';
+import Icon from '../components/Icon';
+
+const user = {
+    name: "Augustine Awuori",
+    email: "augustineawuori95@gmail.com",
+};
+
+const menuItems = [
+    {
+        title: "My Reviews",
+        icon: {
+            name: "email",
+            backgroundColor: colors.secondary,
+        },
+    }
+];
+
+export default function AccountScreen({ }) {
+    return (
+        <Screen style={styles.screen}>
+            <View style={styles.container}>
+                <ListItem
+                    title={user.name}
+                    subTitle={user.email}
+                    image={require("../../assets/user.png")}
+                />
+            </View>
+            <View style={styles.container}>
+                <FlatList
+                    data={menuItems}
+                    keyExtractor={item => item.title}
+                    ItemSeparatorComponent={ListItemSeparator}
+                    renderItem={({ item }) => (
+                        <ListItem
+                            IconComponent={<Icon name={item.icon.name} backgroundColor={item.icon.backgroundColor} />}
+                            title={item.title}
+                            onPress={() => { }}
+                        />
+                    )}
+                />
+            </View>
+            <ListItem
+                title='Log out'
+                IconComponent={<Icon name='logout' backgroundColor="#ffe66d" />}
+                onPress={() => { }}
+            />
+        </Screen>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        marginBottom: 20,
+    },
+    screen: {
+        backgroundColor: colors.light,
+    },
+});
